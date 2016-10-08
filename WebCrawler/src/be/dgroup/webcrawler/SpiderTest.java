2c4049d7fd94287796effabb17a98343cd8881ca
MBP <MBP@mbpusers-mbp.home>
2016-10-08 17:03:51 +0200

    3 klassen // klaar

--- WebCrawler/src/be/dgroup/webcrawler/SpiderTest.java
+++ WebCrawler/src/be/dgroup/webcrawler/SpiderTest.java
@@ -20,6 +20,6 @@ public class SpiderTest
     public static void main(String[] args)
     {
         Spider spider = new Spider();
-        spider.search("http://arstechnica.com/", "computer");
+        spider.search("http://www.hln.be/", "computer");
     }
 }
