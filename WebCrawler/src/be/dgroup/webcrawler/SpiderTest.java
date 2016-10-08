4f7a211f9f2ff5632ec4286a918856d57aeeff04
MBP <MBP@mbpusers-mbp.home>
2016-10-08 16:59:13 +0200

    3 klassen // klaar

---
+++ WebCrawler/src/be/dgroup/webcrawler/SpiderTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.dgroup.webcrawler;
+
+/**
+ *
+ * @author DiMilan <milan@dgroup.be>
+ */
+public class SpiderTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     * 
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://arstechnica.com/", "computer");
+    }
+}
